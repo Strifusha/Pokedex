44232c2fb77c8a08ea64ab85d41936b94d697c79
Strifusha <KatushaStrifusha@gmail.com>
2023-05-29 23:11:48 +0300

    added section with chosen pokemon

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//получить данные с сервера
 // создать див-контейнер с фото, именем, кнопкой
 // вставить покемонов в ячейки через итерацию массива
 //создать массив с именами покемонов для forEach
@@ -6,8 +5,6 @@
 // повесить ивет лисенер на кнопки под фото
 
 getPokemons();
-let pokemonsInfo = [];
-
 
 function getPokemons(){
     var url = 'https://pokeapi.co/api/v2/pokemon/?limit=12'
@@ -15,7 +12,6 @@ function getPokemons(){
         .then(responseStatus)
         .then(json)
         .then(function(data){
-            //console.log(data.results)
             getInfoByUrl(data.results);
         })
         .catch(function(err){
@@ -30,8 +26,6 @@ async function getInfoByUrl(shortPokemonList) {
 
     let pokemonUrs = shortPokemonList.map(item => item.url)
 
-    console.log('pokemonUrs--> ', pokemonUrs);
-
     const pokemonData = await Promise.all(
         pokemonUrs.map(async (url) => {
           const response = await fetch(url);
@@ -39,11 +33,8 @@ async function getInfoByUrl(shortPokemonList) {
           return pokemonDetails;
         })
       );
-
-        console.log('pokemonData', pokemonData)
     
     renderPokemons(pokemonData);   
-   
 }
 
 function responseStatus(response){
@@ -57,19 +48,82 @@ function json(response){
     return response.json();
 }
 
-
 function renderPokemons(renderedPokemons){
     const getGridSection = document.getElementById('pokemons-container');
     getGridSection.innerHTML = '';
     let allPokemons = '';
-   
+    
     for(let i = 0; i < renderedPokemons.length; i++){
+        //console.log(renderedPokemons[i])
+        // const types = renderedPokemons[i].types.flatMap(obj => Object.values(obj.type.name));
+        // for(let i = 0; i < types.length; i++){
+        //     console.log(types)
+        // }
+        ///////
+        //const types = renderPokemons[i].types.map(obj => Object.values(obj)[0].type.name);
+        //console.log(types)
+
         
           allPokemons += `<div class='grid-item'>
-                              <img src="${renderedPokemons[i].sprites.front_default}" alt="">
-                              <h4 class='pokemon-name'>${renderedPokemons[i].name}</h4>
+                              <img src="${renderedPokemons[i].sprites.front_default}" class='pokemon-img' alt="Pokemon's pic">
+                              <h3 class='pokemon-name'>${renderedPokemons[i].name}</h3>
+                              <span class='pokemonAbility'>${renderedPokemons[i].types[0].type.name}</span>
                           </div>`;
-         getGridSection.innerHTML = allPokemons; 
+         getGridSection.innerHTML = allPokemons;
     }
+
+    //adding colours to abilities
+    const pokemonTypes = document.getElementsByClassName('pokemonAbility');
+
+    for(let i = 0; i < pokemonTypes.length; i++){
+
+        if(pokemonTypes[i].innerHTML == 'grass'){
+            pokemonTypes[i].style.backgroundColor = 'rgb(31, 217, 155)';
+        }
+        if(pokemonTypes[i].innerHTML == 'fire'){
+            pokemonTypes[i].style.backgroundColor = 'rgb(243, 40, 40)';
+        }
+        if(pokemonTypes[i].innerHTML == 'poison'){
+            pokemonTypes[i].style.backgroundColor = 'rgb(126, 34, 238)';
+        }
+        if(pokemonTypes[i].innerHTML == 'electric'){
+            pokemonTypes[i].style.backgroundColor = 'rgb(237, 250, 53)';
+        }
+        if(pokemonTypes[i].innerHTML == 'water'){
+            pokemonTypes[i].style.backgroundColor = 'aqua';
+        }
+        if(pokemonTypes[i].innerHTML == 'bug'){
+            pokemonTypes[i].style.backgroundColor = 'rgb(129, 87, 87)';
+        }
+    }
+    showPokemonDetails()
 }
+
+    
+function showPokemonDetails(){
+    //убрать бордер пока не выбран покемон в начале
+    //прокручивать табло с прокруткой вниз всех покемонов
+
+    const getGridItems = document.querySelectorAll('.grid-item');
+    const infoArea = document.getElementById('big-pokemon-section');
+    infoArea.innerHTML = '';
+    let moreDatails ='';
+
+    getGridItems.forEach(item => {
+        item.addEventListener('click', () => {
+            
+            //console.log(renderPokemons[item].name);
+            
+            if(item) { 
+                //console.log(item.children);
+                moreDatails = `<img src='${item.children[0].src}' class="pokemon-big-img" alt="Pokemon's pic">
+                <h2 class='pokemon-name'>${item.children[1].outerText}</h2>
+                `
+                infoArea.innerHTML = moreDatails; 
+
+            }     
+        })  
+    })    
+}
+
 
